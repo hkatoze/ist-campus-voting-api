e60ab2abf04b4ca2edc6d2dd964ca62166639d6a
Haroun <harounakinda.pro@gmail.com>
2025-12-11 12:46:14 +0000

    sendgrid

--- src/utilsFunctions/mailer.js
+++ src/utilsFunctions/mailer.js
@@ -1,29 +1,35 @@
-const SibApiV3Sdk = require("sib-api-v3-sdk");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const client = SibApiV3Sdk.ApiClient.instance;
-client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
-
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+ 
 
 async function sendOtpEmail(to, code) {
-  try {
-    const sendSmtpEmail = {
-      sender: { email: process.env.FROM_EMAIL, name: "Campus Voting" },
-      to: [{ email: to }],
+
+    const msg = {
+      to: to, 
+      from: process.env.FROM_EMAIL, 
       subject: "Votre code OTP - Vote Campus",
-      htmlContent: `
+      text: "and easy to do anywhere, even with Node.js",
+      html: `
         <p>Bonjour,</p>
         <p>Votre code OTP est : <strong>${code}</strong></p>
         <p>Il expire dans 1H.</p>
       `,
     };
+    sgMail
+      .send(msg)
+      .then(() => {
+       console.log("Sengrid email sent:", result);
+      })
+      .catch((error) => {
+ console.error("Sengrid ERROR:", error.response?.body || error);
+ throw new Error("Erreur lors de l’envoi du mail OTP");
+      });
 
-    const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    console.log("Brevo email sent:", result);
-  } catch (error) {
-    console.error("BREVO ERROR:", error.response?.body || error);
-    throw new Error("Erreur lors de l’envoi du mail OTP");
-  }
+
+
 }
 
 module.exports = { sendOtpEmail };
+
+ 
