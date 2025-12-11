a1c101aa019cf359a6ffa261c564b0a33921b10e
Haroun <harounakinda.pro@gmail.com>
2025-12-11 03:53:20 +0000

    brevo

--- src/utilsFunctions/mailer.js
+++ src/utilsFunctions/mailer.js
@@ -1,19 +1,43 @@
-const nodemailer = require("nodemailer");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: parseInt(process.env.SMTP_PORT || "587", 10),
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+const client = SibApiV3Sdk.ApiClient.instance;
+client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY; 
+
+const emailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
 async function sendOtpEmail(to, code) {
-  const from = process.env.FROM_EMAIL;
-  const subject = "Code OTP - Vote Campus";
-  const text = `Votre code de connexion est : ${code}\nIl expire dans 1H.`;
-  //await transporter.sendMail({ from, to, subject, text });
+  const sender = {
+    email: process.env.FROM_EMAIL,
+    name: "Vote IST AZIMMO",
+  };
+
+  const subject = "Votre code OTP • Campus Voting";
+
+  const textContent = `Votre code de connexion est : ${code}\nIl expire dans 1H.`;
+
+  const htmlContent = `
+    <h2>Code OTP</h2>
+    <p>Votre code de connexion est :</p>
+    <div style="font-size:22px; font-weight:bold; letter-spacing:3px;">
+      ${code}
+    </div>
+    <p>Il expire dans <b>1H</b>.</p>
+  `;
+
+  try {
+    await emailApi.sendTransacEmail({
+      sender,
+      to: [{ email: to }],
+      subject,
+      textContent,
+      htmlContent,
+    });
+
+    console.log("OTP email sent to:", to);
+  } catch (error) {
+    console.error("Brevo email error:", error);
+    throw new Error("Erreur lors de l’envoi du mail OTP");
+  }
 }
 
 module.exports = { sendOtpEmail };
