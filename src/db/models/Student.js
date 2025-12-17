59fa4c9d8e4c4ccf457d71056aa8cb0cce0c6b64
Haroun <harounakinda.pro@gmail.com>
2025-12-17 19:11:20 +0000

    kjklkjkl

--- src/db/models/Student.js
+++ src/db/models/Student.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require("sequelize");
-
 module.exports = (sequelize) => {
   const Student = sequelize.define("Student", {
     id: {
@@ -7,37 +6,15 @@ module.exports = (sequelize) => {
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
-
-    matricule: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: true,
-      validate: {
-        isEmail: {
-          msg: "Entrer un mail valide",
-        },
-      },
-    },
-
+    matricule: { type: DataTypes.STRING, unique: true, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: true },
     nom: { type: DataTypes.STRING },
     prenom: { type: DataTypes.STRING },
     filiere: { type: DataTypes.STRING },
-
-    has_voted: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
+    has_voted: { type: DataTypes.BOOLEAN, defaultValue: false },
   });
-
   Student.associate = (models) => {
     Student.hasOne(models.Vote, { foreignKey: "student_id", as: "vote" });
   };
-
   return Student;
 };
